44624e97126ef325e9bc1a1790c46135d5b46d01
ColdCoder92 <lucasrodriguez1002@proton.me>
2025-05-23 12:00:50 -0400

    Setup Application Patch 1

--- Preplit.API/Program.cs
+++ Preplit.API/Program.cs
@@ -1,8 +1,154 @@
-var builder = WebApplication.CreateBuilder(args);
+using System.Text;
+using Preplit.API;
+using Preplit.Data;
+using Preplit.Domain;
+using Preplit.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore;
+using System.Security.Claims;
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
+var builder = WebApplication.CreateBuilder(args);
+var connectionString =
+    builder.Configuration.GetConnectionString("DefaultConnection")
+        ?? throw new InvalidOperationException("Connection string"
+        + "'DefaultConnection' not found.");
+
+builder.Services.AddDbContext<PreplitContext>(options =>
+    options.UseSqlServer(connectionString, x => x.UseNetTopologySuite()));
+
+builder.Services.AddControllers();
+
+builder.Services.AddAutoMapper(typeof(MappingProfiles).Assembly);
+builder.Services.AddMediatR(options =>
+{
+
+    options.RegisterServicesFromAssemblyContaining<GetActivityList.Handler>();
+
+});
+
 builder.Services.AddOpenApi();
+
+builder
+    .Services.AddIdentityCore<User>(options =>
+    {
+        options.Lockout.AllowedForNewUsers = false;
+        options.Password.RequireDigit = true;
+        options.Password.RequiredLength = 8;
+        options.Password.RequireNonAlphanumeric = false;
+        options.Password.RequireUppercase = true;
+    })
+    .AddRoles<IdentityRole<Guid>>()
+    .AddEntityFrameworkStores<PreplitContext>()
+    .AddSignInManager()
+    .AddRoleManager<RoleManager<IdentityRole<Guid>>>();
+
+SymmetricSecurityKey key = new(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]));
+
+builder
+    .Services.AddAuthentication(options =>
+    {
+        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    })
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = key,
+            ValidateIssuer = true,
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidateAudience = true,
+            ValidAudience = builder.Configuration["Jwt:Audience"],
+            ValidateLifetime = true,
+            ClockSkew = TimeSpan.Zero,
+            NameClaimType = ClaimTypes.Name,
+            RoleClaimType = ClaimTypes.Role,
+        };
+
+        options.Events = new JwtBearerEvents
+        {
+            OnMessageReceived = ctx =>
+            {
+                // grab the cookie named "jwt" and then User.Identity?.IsAuthenticated should work
+                if (ctx.Request.Cookies.TryGetValue("jwt", out var token))
+                    ctx.Token = token;
+                return Task.CompletedTask;
+            },
+        };
+    });
+
+
+builder.Services.AddAuthorization();
+
+//Services
+
+
+
+
+//swagger
+//Adding swagger support
+builder.Services.AddEndpointsApiExplorer();
+
+//Modifying this AddSwaggerGen() call to allow us to test/debug our Auth scheme setup in swagger
+builder.Services.AddSwaggerGen(c =>
+{
+    c.AddSecurityDefinition(
+        "Bearer",
+        new OpenApiSecurityScheme
+        {
+            Description = "JWT Authorization header using the Bearer scheme",
+            Type = SecuritySchemeType.Http,
+            Scheme = "bearer"
+        }
+    );
+    c.AddSecurityRequirement(
+        new OpenApiSecurityRequirement
+        {
+            {
+                new OpenApiSecurityScheme
+                {
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    }
+                },
+                Array.Empty<string>()
+            }
+        }
+    );
+});
+
+//Can use http requests
+builder.Services.AddHttpClient();
+
+//cORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowReactApp", policy =>
+    {
+        policy.WithOrigins("http://localhost:5173") // React dev server
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+
+    options.AddPolicy("AllowFrontend", policy =>
+    {
+
+        policy.WithOrigins("https://jaza-bnerbvbkfadkhkbf.canadaeast-01.azurewebsites.net") // azure
+
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+
+});
+
 
 var app = builder.Build();
 
@@ -10,32 +156,58 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    app.UseCors("AllowReactApp");
+    Console.WriteLine("allowed dev");
+    
 }
+else
+{
+    app.UseCors("AllowFrontend");
+
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+
+}
+
+
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
 
-app.MapGet("/weatherforecast", () =>
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+
+
+app.MapControllers();
+
+
+// For first timec
+using (var scope = app.Services.CreateScope())
 {
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+    var services = scope.ServiceProvider;
+
+    try
+    {
+        var context = services.GetRequiredService<PreplitContext>();
+        var userManager = services.GetRequiredService<UserManager<User>>();
+        var roleManager = services.GetRequiredService<RoleManager<IdentityRole<Guid>>>();
+
+        await context.Database.MigrateAsync();
+
+        await Seeder.SeedAdminData(userManager, roleManager);
+        await Seeder.SeedUserData(userManager);
+        await Seeder.SeedCategoryData(userManager, context);
+        await Seeder.SeedCardData(context);
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "Error seeding roles");
+    }
+}
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
